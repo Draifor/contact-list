99893f8a5e7a971e28ff60b025c3c97ae9803f69
Alfredo Parra <alfredo-920@hotmail.com>
2022-03-25 15:57:00 -0500

    Se crearon la clase, la lista y las funciones agregar y eliminar usuario

--- js/scripts.js
+++ js/scripts.js
@@ -7,5 +7,44 @@ Crea una función para imprimir en consola los contactos presentes en la lista
 Publica tu código en Github, en tu repositorio contact-list crea un branch llamado
 project-1 y compártelo con nosotros. 👍🏼⬇️ */
 
-// Definición de variables
+//* Definición de la función para crear objetos Contact
+class Contact {
+    constructor(name, cell) {
+        // Atributos
+        this.name = name;
+        this.cell = cell;
+    }
+}
 
+//* Lista con algunos contactos predefinidos
+const contactList = [
+    new Contact("Anastasia", 3500000000),
+    new Contact("Petronio", 3100000000),
+    new Contact("Jacinta", 3120000000),
+    new Contact("Rupertino", 3200000000)
+];
+// FUNCIONES
+//* Añadir contacto - Recibe dos argumentos, nombre y celular del nuevo contacto
+const addContact = function(name, cell) {
+    contactList.push(new Contact(name, cell));
+}
+
+//* Borrar contacto - Recibe como argumento el nombre del contacto a eliminar
+const delContact = function(name) {
+    for (let i = 0; i < contactList.length; i++) {
+        if (contactList[i].name == name) {
+            let confirmation = prompt(`Eliminar contacto:
+                                        \nNombre: ${contactList[i].name}
+                                        \ncelular: ${contactList[i].cell}
+                                        \n¿Está seguro? ('S' para confirmar, 'N' para abortar)`);
+            confirmation = confirmation.toUpperCase();
+            if (confirmation == "S") {
+                contactList.splice(i, 1)
+                console.log(`Contacto eliminado exitosamente.`);
+            } else if (confirmation == "N")
+                console.log("Se abortó la operación");
+            else
+                console.log("Opción inválida");
+        }
+    }
+}
